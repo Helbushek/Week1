91a1cb024ff8feb48ec9c99af5336994072514a0
Helbushek <gorevaya134@gmail.com>
2024-05-11 18:04:41 +0700

    added insertion user information to database

--- Eatly/app/Http/Controllers/AuthenticationController.php
+++ Eatly/app/Http/Controllers/AuthenticationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
+use DB;
 
 
 use Illuminate\Database\Schema\Blueprint;
@@ -12,15 +13,15 @@ use Illuminate\Database\Schema\Blueprint;
 class AuthenticationController extends BaseController
 {
     public static function sign_up(Request $request): RedirectResponse {
-        $user = $request->query;
-        print_r($user);
        $validated = $request->validate([
             'name' =>  'required|min:5|max:255',
             'email' => 'required|email|unique:users',
             'password' => 'required|confirmed|min:6', 
        ]);
 
-       return redirect('auth/sign_in');
+       DB::insert('insert into users (name, email, password) values (?,?,?)', [$request->name, $request->email, $request->password]);
+
+       return redirect('user');
     }
 
     public static function sign_in(Request $request): RedirectResponse{
